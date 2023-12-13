658a6dacbb1ee430ebf51d53ab455e5231ebff68
Fatima <f4t11ysblu@gmail.com>
2023-12-13 22:11:04 +0400

    js task

---
+++ task/index.js
@@ -0,0 +1,88 @@
+// task1
+// let a=prompt("a ededi daxil edin:");
+// if(a>=0){
+//     console.log("a musbet eddedir");
+// }else{
+//     console.log("a menfi eddedir");
+// }
+
+
+// task2
+// const a=5;
+// const b=6;
+// const c=7;
+// if(a<b+c & a>Math.abs(b-c)){
+// if(b<a+c & b>Math.abs(a-c)){
+//     if(c<a+b & c>Math.abs(a-b)){
+//         const yariPerimetr=(a+b+c)/2;
+// const Sahe=Math.sqrt(yariPerimetr*(yariPerimetr-a)*(yariPerimetr-b)*(yariPerimetr-c));
+// console.log("Ucbucagin sahesi:" ,Sahe);
+//     }
+// }
+// }else{
+//     console.log("Bele bir ucbucaq movcud deyil");
+// }
+
+
+// task3
+// let a=prompt("Bir eded daaxil edin");
+// let x=13;
+
+
+// if(a>13){
+//     let sum1=(a-13)*2;
+//     console.log(sum1);
+// }else{
+//     let sum2=(a-13);
+//     console.log(sum2);
+
+// }
+
+
+// task4
+// let a=prompt("a ededini daxil edin");
+// let b=prompt("b ededini daxil edin");
+//   let sum=parseInt(a)+parseInt(b);
+// if(a===b){
+// console.log(sum);
+// console.log(sum);
+// console.log(sum);
+// }else{
+//     console.log(sum);
+// }
+
+
+// task5
+// var x = prompt("bir eded daxil edin");
+// var y = prompt("bir eded daxil edin");
+
+// var sum = parseInt(x) + parseInt(y);
+
+// if ((parseInt(x) % 2 === 0) && (parseInt(y) % 2 === 0)) {
+//     console.log("her ikis cutdur");
+    
+//  if (sum === 50 || parseInt(x) === 50 || parseInt(y) === 50) {
+//         console.log("true");
+//  } else {
+//         console.log("false");
+//  }
+// } else {
+//     console.log("hec biri cut deyil");
+// }
+
+
+// task6
+
+
+
+// Task7
+// let x=prompt("Birinci ededi daxil edin");
+// let y=prompt("Ikinci ededi daxil edin");
+
+// if((x>=50 && x<=99) || (y>=50 && y<=99)){
+//     alert("ededlerden biri bu diapozondadir");
+// }else{
+//     alert("ededlerden hec biri bu diapozonda deyil");
+// }
+
+
